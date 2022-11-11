81243712c29b257e5ccf698d89eb5fd954542d54
Alina Potapchuk <alinapotapchuk7@gmail.com>
2022-11-11 19:58:48 +0300

    реализовать попап

--- sripts/script.js
+++ sripts/script.js
@@ -1,6 +1,7 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseBtn = popupElement.querySelector('.popup__close');
-const popupOpenBtn = document.querySelector('.profile__edit-button');
+let profileElement = document.querySelector('.profile');
+const popupOpenBtn = profileElement.querySelector('.profile__edit-button');
 
 const openPopup = function() {
   popupElement.classList.add('popup_opened');
@@ -12,3 +13,17 @@ const closePopup = function() {
 
 popupOpenBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
+
+let popupInputs = popupElement.querySelectorAll('.popup__input');
+let profileName = profileElement.querySelector('.profile__name');
+let profileAbout = profileElement.querySelector('.profile__about');
+popupInputs[0].value = profileName.textContent;
+popupInputs[1].value = profileAbout.textContent;
+
+const saveBtn = popupElement.querySelector('.popup__save');
+saveBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    profileName.textContent = popupInputs[0].value;
+    profileAbout.textContent = popupInputs[1].value;
+    closePopup();
+});
